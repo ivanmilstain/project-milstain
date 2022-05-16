b3627939f3254c28af4eed11d12e08cf15ffb9ce
Iván Milstain <ivan.milstain@gmail.com>
2022-05-16 18:50:04 -0300

    Se integra SQLLite

--- screens/QRScanner.js
+++ screens/QRScanner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { useFocusEffect } from "@react-navigation/native";
